85eb2546321d751eddaedb6dc21b74dbbe2735dc
Timm Heuss <heussd@users.noreply.github.com>
2019-04-06 14:50:35 +0200

    Changed log level to something < DEBUG

--- newsboat.sh
+++ newsboat.sh
@@ -14,7 +14,7 @@ while :; do
   echo "Copying database..."
   cp -fv $newsDbToCopy /data/news.db ;
 
-  newsboat -u /data/urls -d /dev/fd/1 -l 5 -x reload;
+  newsboat -u /data/urls -d /dev/fd/1 -l 4 -x reload;
 
   echo "Copying database back..."
   cp -fv /data/news.db $newsDbToCopy;
