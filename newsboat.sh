78e6aa1070c6ba6413547c364083a3981a4dd355
Timm Heuss <heussd@users.noreply.github.com>
2019-04-08 21:54:23 +0200

    Changed backup strategy, news.db now get copied every full run

--- newsboat.sh
+++ newsboat.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-newsDbToCopy=/data-to-copy/news.db
+newsDbToCopy=/data/news.db
 
 if [ ! -w "$newsDbToCopy" ]; then
    echo "No write access to file $newsDbToCopy"
@@ -11,13 +11,9 @@ dockerize -wait http://fullfeedrss:80
 if [ $? -ne 0 ]; then exit 1; fi
 
 while :; do
-  echo "Copying database..."
-  cp -fv $newsDbToCopy /data/news.db ;
+  newsboat -u /data/urls -d /dev/fd/1 -l 5 -x reload;
 
-  newsboat -u /data/urls -d /dev/fd/1 -l 4 -x reload;
-
-  echo "Copying database back..."
-  cp -fv /data/news.db $newsDbToCopy;
+  cp -fv /data/news.db /data/news.bck.db;
 
   echo \"Waiting $RELOAD_EVERY to reload...\" ;
   sleep $RELOAD_EVERY;
